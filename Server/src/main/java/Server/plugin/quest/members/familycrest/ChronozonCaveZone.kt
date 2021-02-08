db5d9a47c36e6a17f6c73b9e56ebc33950c49038
hfarhat <hfarhat@playingforward.com>
2021-02-08 12:07:39 +1100

    #587 Chronozon spawn logic and fight mechanics coded

--- Server/src/main/java/Server/plugin/quest/members/familycrest/ChronozonCaveZone.kt
+++ Server/src/main/java/Server/plugin/quest/members/familycrest/ChronozonCaveZone.kt
@@ -3,15 +3,20 @@ package plugin.quest.members.familycrest
 
 import core.game.node.entity.Entity
 import core.game.node.entity.combat.ImpactHandler
+import core.game.node.entity.npc.NPC
 import core.game.node.entity.player.Player
 import core.game.system.SystemLogger
+import core.game.system.config.NPCSpawner
 import core.game.system.task.Pulse
 import core.game.world.GameWorld
 import core.game.world.map.Direction
 import core.game.world.map.Location
+import core.game.world.map.Region
+import core.game.world.map.RegionManager
 import core.game.world.map.zone.MapZone
 import core.game.world.map.zone.ZoneBorders
 import core.game.world.map.zone.ZoneBuilder
+import core.game.world.map.zone.ZoneMonitor
 import core.game.world.update.flag.context.Animation
 import core.game.world.update.flag.context.Graphics
 import core.plugin.InitializablePlugin
@@ -22,6 +27,7 @@ import core.tools.RandomFunction
 class ChronozonCaveZone: MapZone("FC ChronozoneZone", true), Plugin<Unit> {
 
     val triggers = ArrayList<Location>()
+    var chronozon = ChronozonNPC(667, Location(3086, 9936, 0))
     override fun configure() {
         register(ZoneBorders(3082, 9929, 3091, 9940))
         triggers.add(Location.create(3083, 9939))
@@ -37,7 +43,15 @@ class ChronozonCaveZone: MapZone("FC ChronozoneZone", true), Plugin<Unit> {
     override fun move(e: Entity?, from: Location?, to: Location?): Boolean {
         if(triggers.contains(from) && e is Player){
             if(e.direction == Direction.getDirection(Location.create(0,0,0), Location.create(0, -1,0))){
-                e.debug("HAZA")
+
+                chronozon = ChronozonNPC(667, Location(3086, 9936, 0))
+
+                if(e.questRepository.getQuest("Family Crest").getStage(e) == 19 && !RegionManager.getLocalNpcs(Location(3086, 9936, 0), 5).contains(chronozon) ){
+                    chronozon.setPlayer(e);
+                    chronozon.isRespawn = false
+                    chronozon.init()
+                    e.debug("Spanwed Chronozon")
+                }
             }
             else {
 
--- Server/src/main/java/Server/plugin/quest/members/familycrest/ChronozonNPC.kt
+++ Server/src/main/java/Server/plugin/quest/members/familycrest/ChronozonNPC.kt
@@ -1,17 +1,45 @@
 package plugin.quest.members.familycrest
 
 import core.game.node.entity.Entity
+import core.game.node.entity.combat.BattleState
 import core.game.node.entity.combat.CombatStyle
+import core.game.node.entity.combat.DeathTask
+import core.game.node.entity.combat.ImpactHandler
 import core.game.node.entity.npc.AbstractNPC
 import core.game.node.entity.player.Player
 import core.game.node.entity.player.link.quest.Quest
 import core.game.world.map.Location
 import core.game.world.map.RegionManager
 
+import core.game.content.global.BossKillCounter;
+import core.game.node.entity.combat.CombatSwingHandler;
+import core.game.node.entity.combat.InteractionType;
+import core.game.node.entity.combat.equipment.ArmourSet;
+import core.game.node.entity.combat.equipment.SpellType
+import core.game.node.entity.impl.Projectile;
+import core.game.node.entity.impl.Animator.Priority;
+
+import core.game.node.entity.npc.NPC;
+
+import core.game.system.task.Pulse;
+import core.game.world.GameWorld;
+
+import core.game.world.update.flag.context.Animation;
+import core.game.world.update.flag.context.Graphics;
+import core.plugin.Plugin;
+import core.tools.RandomFunction;
 
 class ChronozonNPC(id: Int, location: Location?) : AbstractNPC(667, Location(3086, 9936, 0)){
 
-    var targetPlayer: Player? = null
+    lateinit var m_targetPlayer: Player
+
+    var m_amountOfFireDamageTaken: Int = 0
+
+    var m_amountOfAirDamageTaken: Int = 0
+
+    var m_amountOfWaterDamageTaken: Int = 0
+
+    var m_amountOfEarthDamageTaken: Int = 0
 
     override fun construct(id: Int, location: Location?, vararg objects: Any?): AbstractNPC {
         return ChronozonNPC(id, location)
@@ -23,13 +51,80 @@ class ChronozonNPC(id: Int, location: Location?) : AbstractNPC(667, Location(308
 
     override fun handleTickActions() {
         super.handleTickActions()
-        if(RegionManager.getLocalPlayers(this).contains(targetPlayer)){
+        if (!m_targetPlayer.isActive || m_targetPlayer.getLocation().getDistance(getLocation()) > 15) {
+            clear()
+        }
+
+    }
+
+    override fun checkImpact(state: BattleState?) {
+        if (state != null) {
+            if(state.style != CombatStyle.MAGIC){
+                state.neutralizeHits()
+            }
+            else{
+                if(state.spell.spellId == 24 ||state.spell.spellId == 45) {
+                    if (m_amountOfAirDamageTaken < 25) {
+                        m_amountOfAirDamageTaken += state.estimatedHit
+                    } else {
+                        state.neutralizeHits()
+                        m_targetPlayer.sendMessage("Air Blast seems to have done all it will to Chronozon")
+                    }
+                }
+
+                if(state.spell.spellId == 33 ||state.spell.spellId == 52) {
+                    if (m_amountOfEarthDamageTaken < 25) {
+                        m_amountOfEarthDamageTaken += state.estimatedHit
+                    }
+                    else {
+                        state.neutralizeHits()
+                        m_targetPlayer.sendMessage("Earth Blast seems to have done all it will to Chronozon")
+                    }
+                }
+
+                if(state.spell.spellId == 38 || state.spell.spellId == 55) {
+                    if (m_amountOfFireDamageTaken < 25) {
+                        m_amountOfFireDamageTaken += state.estimatedHit
+                    } else {
+                        state.neutralizeHits()
+                        m_targetPlayer.sendMessage("Fire Blast seems to have done all it will to Chronozon")
+                    }
+                }
+
+                if(state.spell.spellId == 27 ||state.spell.spellId == 48)
+                    if(m_amountOfWaterDamageTaken < 25){
+                        m_amountOfWaterDamageTaken += state.estimatedHit
+                    }
+                    else{
+                        state.neutralizeHits()
+                        m_targetPlayer.sendMessage("Water Blast seems to have done all it will to Chronozon")
+                    }
+
+            }
 
         }
     }
 
+    override fun isAttackable(entity: Entity, style: CombatStyle?): Boolean {
+        return if (entity != m_targetPlayer) {
+            false
+        }
+
+        else super.isAttackable(entity, style)
+    }
+
+    override fun clear() {
+        super.clear()
+    }
+
+    override fun finalizeDeath(killer: Entity?) {
+        clear()
+        super.finalizeDeath(killer)
+
+    }
+
     fun setPlayer(player: Player){
-        targetPlayer = player;
+        m_targetPlayer = player;
     }
 
 }
